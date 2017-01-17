86e7489af4fa2c235306241f03d5a16763362ec1
Ana Lucia <analucia.carhuaz@cloudware360.com>
2017-01-17 18:40:03 -0500

    css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 window.addEventListener("load", function(){
 	// variables globales
-	
+	var lista = document.getElementById("list");
+	var newList	= document.getElementById("newList")
+	var listName = document.getElementById("listName");
+
+	lista.addEventListener("click", NewList);
+
+	function NewList(){
+		lista.classList.add("hidden");
+	    newList.classList.remove("hidden");
+	    listName.classList.add("block");
+	    listName.focus();
+	}
+
 });
 // validar inut vacio 
 function validarInput(){
