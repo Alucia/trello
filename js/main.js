3dd3621fc82331d54ec703d27e9dfd1c96ef240f
Alucia <analu1478@gmail.com>
2017-01-18 00:09:15 -0500

    css seccion newList

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", function(){
 	// variables globales
 	var lista = document.getElementById("list");
-	var newList	= document.getElementById("newList")
+	var newList	= document.getElementById("newList");
 	var listName = document.getElementById("listName");
+	var wrapperList = document.getElementById("wrapperList");
 
 	lista.addEventListener("click", NewList);
 
-	function NewList(){
+	function NewList(e){
 		lista.classList.add("hidden");
 	    newList.classList.remove("hidden");
+	    wrapperList.classList.add("addList");
 	    listName.classList.add("block");
+	    listName.classList.add("inputAdd");
 	    listName.focus();
 	}
 
