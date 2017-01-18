49d8999b01295f108772527f84b02aa57590d19a
Alucia <analu1478@gmail.com>
2017-01-18 00:20:07 -0500

    js validando nombre de lista vacio

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function(){
 	var wrapperList = document.getElementById("wrapperList");
 
 	lista.addEventListener("click", NewList);
-
+	//ingresar nombre de lista
 	function NewList(e){
 		lista.classList.add("hidden");
 	    newList.classList.remove("hidden");
@@ -15,14 +15,14 @@ window.addEventListener("load", function(){
 	    listName.classList.add("inputAdd");
 	    listName.focus();
 	}
-
+	// validar inut vacio 
+	function validarInput(){
+	    var contenido = listName.value.length;
+	    if(contenido <= 0){
+	        botonGuardar.disabled = true;
+	    }else if(contenido >= 1){
+	        botonGuardar.disabled = false;
+	    } 
+	}
+	
 });
-// validar inut vacio 
-function validarInput(){
-    var contenido = inputName.value.length;
-    if(contenido <= 0){
-        botonGuardar.disabled = true;
-    }else if(contenido >= 1){
-        botonGuardar.disabled = false;
-    } 
-}
